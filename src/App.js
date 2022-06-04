06072d03bdb4c5a3db03258de510e82c851467b4
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2022-06-04 23:52:16 +0300

    style-definition example

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div style={{ color: "red", backgroundColor: "white", paddingTop: 50 }}>
+          Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır
+          metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat
+          numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı
+          1500'lerden beri endüstri standardı sahte metinler olarak
+          kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış,
+          aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır.
+        </div>
+        <div class="alert alert-primary" role="alert">
+          A simple primary alert—check it out!
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
