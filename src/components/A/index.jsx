6ec7fbed7bad5951e0ad5d67a51344713fff54d5
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2022-06-05 22:47:56 +0300

    Module CSS

---
+++ src/components/A/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+const A = () => {
+  return (
+    <div className={styles.title}>
+      AAAAA
+      <div className={styles.header}>aaaaaaa</div>
+    </div>
+  );
+};
+
+export default A;
---
+++ src/components/B/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+const B = () => {
+  return <div className={styles.title}>BBBB</div>;
+};
+
+export default B;
